eda686a0fcb7c4be833953bb0f50ea53c6429a29
lololibc <160233179+lololibc@users.noreply.github.com>
2025-10-14 23:20:01 +0000

    exercicio 1 completo

--- App.java
+++ App.java
@@ -11,65 +11,27 @@ public class App {
         // Estruturas condicionais: if, else if, else, switch
         // Estruturas de repetição: for, while, do while
 
-        // exercício da aula 7
-        int opcao;
-        do {
-            System.out.println(
-                    "Opções do Menu:\n1 - Opção 1: Soma\n2 - Opção 2: Subtração\n3 - Opção 3: Multiplicação\n4 - Opção 4: Divisão\n5 - Opção 5: Sair");
-            System.out.print("Digite um número entre 1 e 5: ");
-            opcao = leitor.nextInt();
+        // exercício da aula 8
+        // exercicio 1
+        String senha = "123";
+        String entrada;
+        int tentativas = 0;
 
-            switch (opcao) {
-                case 1:
-                    System.out.println("Você escolheu a opção 1: Soma");
-                    System.out.print("Entre com o primeiro número: ");
-                    double num1 = leitor.nextInt();
-                    System.out.print("Entre com o segundo número: ");
-                    double num2 = leitor.nextInt();
-                    double soma = num1 + num2;
-                    System.out.println("A soma de " + num1 + " + " + num2 + " = " + soma);
-                    break;
-                case 2:
-                    System.out.println("Você escolheu a opção 2: Subtração");
-                    System.out.println("Você escolheu a opção 2: Subtração");
-                    System.out.print("Entre com o primeiro número: ");
-                    num1 = leitor.nextInt();
-                    System.out.print("Entre com o segundo número: ");
-                    num2 = leitor.nextInt();
-                    double subtracao = num1 - num2;
-                    System.out.println("A diferença de " + num1 + " - " + num2 + " = " + subtracao);
-                    break;
-                case 3:
-                    System.out.println("Você escolheu a opção 3: Multiplicação");
-                    System.out.print("Entre com o primeiro número: ");
-                    num1 = leitor.nextInt();
-                    System.out.print("Entre com o segundo número: ");
-                    num2 = leitor.nextInt();
-                    double multiplicacao = num1 * num2;
-                    System.out.println("O produto de " + num1 + " * " + num2 + " = " + multiplicacao);
-                    break;
-                case 4:
-                    System.out.println("Você escolheu a opção 4: Divisão");
-                    System.out.print("Entre com o primeiro número: ");
-                    num1 = leitor.nextInt();
-                    System.out.print("Entre com o segundo número: ");
-                    num2 = leitor.nextInt();
-                    if (num2 == 0) {
-                        System.out.println("1");
-                    } else {
-                        System.out.println("0");
-                    }
-                    break;
-                case 5:
-                    System.out.println("Você escolheu a opção 5: Sair");
-                    break;
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
-                    break;
+        boolean continua = true;
+        
+        while (continua && tentativas < 3){
+            entrada = leitor.next();
+            if (entrada.equals(senha)) {
+
+                System.out.println("Senha correta!");
+                continua = false;
+            } else {
+                System.out.println("Senha incorreta!");
             }
 
-        } while (opcao != 5);
-
+            tentativas = tentativas + 1;
+        }
+        
         leitor.close();
 
     }
